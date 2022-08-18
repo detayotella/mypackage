e5618743c8278dc3828026b22868cdff7a65d4fc
Tella Adetayo <tellaadetayo7@yahoo.com>
2022-08-18 05:51:50 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mypackage",
+    version="0.1",
+    packages=find_packages(exclude=["test*"]),
+    license="MIT",
+    description="ESDA example python package",
+    long_description=open("README.md").read(),
+    install_requires=["numpy"],
+    url="https:github.com/detayotella/mypackage",
+    author="Adetayo Tella",
+    author_email="tellaadetayo7@yahoo.com"
+)
